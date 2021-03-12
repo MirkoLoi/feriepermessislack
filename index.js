11673ce6215669139fe84f1483ed678ae90de3d8
Mirko Loi <mirko.loi@apuliasoft.com>
2021-03-12 13:13:03 +0100

    Inserito evnto hello

--- index.js
+++ index.js
@@ -20,6 +20,30 @@ bot.event("app_mention", async ({ context, event }) => {
 
 });
 
+app.message('hello', async ({ message, say }) => {
+    // say() sends a message to the channel where the event was triggered
+    await say({
+      blocks: [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": `Hey there <@${message.user}>!`
+          },
+          "accessory": {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "Click Me"
+            },
+            "action_id": "button_click"
+          }
+        }
+      ],
+      text: `Hey there <@${message.user}>!`
+    });
+  });
+
 (async () => {
   // Start the app
 
