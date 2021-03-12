7c47fc1f69993cccb53dd9d8d515df451bff708b
Mirko Loi <mirko.loi@apuliasoft.com>
2021-03-12 12:28:11 +0100

    Fix dipendenza dotenv

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-require('dotenv').config();
-const { App } = require('@slack/bolt');
+const SlackBot = require('@slack/bolt');
 
-const bot = new App({
+const bot = new SlackBot({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
 });
