8f224e1df8e774cdd4870d34d36f0d4f9966d9a3
Mirko Loi <mirko.loi@apuliasoft.com>
2021-03-12 14:34:11 +0100

    Insert console.log

--- index.js
+++ index.js
@@ -22,26 +22,8 @@ bot.event("app_mention", async ({ context, event }) => {
 
 bot.message('hello', async ({ message, say }) => {
     // say() sends a message to the channel where the event was triggered
-    await say({
-      blocks: [
-        {
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": `Hey there <@${message.user}>!`
-          },
-          "accessory": {
-            "type": "button",
-            "text": {
-              "type": "plain_text",
-              "text": "Click Me"
-            },
-            "action_id": "button_click"
-          }
-        }
-      ],
-      text: `Hey there <@${message.user}>!`
-    });
+    console.log(message)
+    await say(`Ciao <@${message.user}> :wave:`);
   });
 
 (async () => {
