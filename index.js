f69537076e8354ac08834beef8ac6278710b78bf
Mirko Loi <mirko.loi@apuliasoft.com>
2021-03-19 12:57:12 +0100

    Inserito evento di ascolto della submit

--- index.js
+++ index.js
@@ -76,6 +76,9 @@ bot.command("/ferie", async ({ ack, body, client }) => {
             },
           },
           {
+            type: "divider",
+          },
+          {
             type: "input",
             element: {
               type: "multi_users_select",
@@ -96,6 +99,45 @@ bot.command("/ferie", async ({ ack, body, client }) => {
       },
     });
     console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+bot.view("view_b", async ({ ack, body, view, client }) => {
+  // Acknowledge the view_submission event
+  await ack();
+
+  console.log("body", body);
+  console.log("view", view);
+  console.log("client", client);
+
+  await client.views.close();
+
+  // Do whatever you want with the input data - here we're saving it to a DB then sending the user a verifcation of their submission
+
+  // Assume there's an input block with `block_1` as the block_id and `input_a`
+  // const val = view['state']['values']['block_1']['input_a'];
+  const user = body["user"]["id"];
+
+  // // Message to send user
+  let msg = "Bravo";
+  // // Save to DB
+  // const results = await db.set(user.input, val);
+
+  // if (results) {
+  //   // DB save was successful
+  //   msg = 'Your submission was successful';
+  // } else {
+  //   msg = 'There was an error with your submission';
+  // }
+
+  // Message the user
+  try {
+    await client.chat.postMessage({
+      channel: user,
+      text: msg,
+    });
   } catch (error) {
     console.error(error);
   }
