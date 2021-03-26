fe32f870bee457e3cc20c542806164396c87e283
Mirko Loi <mirko.loi@apuliasoft.com>
2021-03-26 11:25:12 +0100

    Fix funzione

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ bot.view("view_submission", async ({ ack, body, view, client }) => {
     viewBlock["holiday-date-init"]["datepicker-action-init"].selected_date
   } a ${viewBlock["holiday-date-end"]["datepicker-action-end"].selected_date}`;
 
-  const result = await client.users_list();
+  const result = await client.users.list();
   console.log(result);
 
   try {
