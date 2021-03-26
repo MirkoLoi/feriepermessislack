8018235330d48b67ca59433c77e2980b6e90fb57
Mirko Loi <mirko.loi@apuliasoft.com>
2021-03-26 11:00:27 +0100

    Richeista lista utenti

--- index.js
+++ index.js
@@ -119,8 +119,8 @@ bot.view("view_submission", async ({ ack, body, view, client }) => {
     viewBlock["holiday-date-init"]["datepicker-action-init"].selected_date
   } a ${viewBlock["holiday-date-end"]["datepicker-action-end"].selected_date}`;
 
-  const result = client.users.list();
-  console.log(result);
+  const result = client.users_list();
+  console.log(result, result["members"]);
 
   try {
     await client.chat.postMessage({
