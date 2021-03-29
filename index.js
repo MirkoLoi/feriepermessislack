eae6bad9a8ac1e4d6eb5087664dac257c7135af0
Mirko Loi <mirko.loi@apuliasoft.com>
2021-03-29 18:18:49 +0200

    test

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ bot.view("view_submission", async ({ ack, body, view, client }) => {
 bot.action("accept_refuse", async ({ ack, payload, body, client }) => {
   await ack();
 
-  console.log(payload, body);
+  console.log(payload);
 });
 
 function capitalizeName(name) {
