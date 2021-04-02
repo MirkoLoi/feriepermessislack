bc773d783e0422a79ecf7786bc2e66c42303de09
Mirko Loi <mirko.loi@apuliasoft.com>
2021-04-02 16:32:06 +0200

    Fix

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ bot.view("view_submission", async ({ ack, body, view, client }) => {
 bot.action("accept_refuse", async ({ ack, payload, body, client }) => {
   await ack();
 
-  console.log(JSON.pase(payload.value));
+  console.log(JSON.parse(payload.value));
 });
 
 function capitalizeName(name) {
