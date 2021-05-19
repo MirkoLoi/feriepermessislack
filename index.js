49a83240279fa9a5a14b3f192e3351f15ddc4b76
Mirko Loi <mirko.loi@apuliasoft.com>
2021-05-19 17:38:22 +0200

    Insert function for calendar

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const { App } = require("@slack/bolt");
+const fs = require("fs");
+const readline = require("readline");
+const { google } = require("googleapis");
 
 const bot = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -276,6 +279,10 @@ async function notifyResponse(client, pmUser, selectedOption) {
       },
     ],
   });
+
+  if (selectedOption.response) {
+    createCalendarEvent();
+  }
 }
 
 function capitalize(name) {
@@ -285,6 +292,114 @@ function capitalize(name) {
   return capital;
 }
 
+function createCalendarEvent() {
+  const SCOPES = ["https://www.googleapis.com/auth/calendar.events"];
+  const TOKEN_PATH = "token.json";
+
+  fs.readFile("credentials.json", (err, content) => {
+    if (err) return console.log("Error loading client secret file:", err);
+    authorize(JSON.parse(content), listEvents);
+  });
+
+  /**
+   * Create an OAuth2 client with the given credentials, and then execute the
+   * given callback function.
+   * @param {Object} credentials The authorization client credentials.
+   * @param {function} callback The callback to call with the authorized client.
+   */
+  function authorize(credentials, callback) {
+    const { client_secret, client_id, redirect_uris } = credentials.web;
+    const oAuth2Client = new google.auth.OAuth2(
+      client_id,
+      client_secret,
+      redirect_uris[0]
+    );
+    fs.readFile(TOKEN_PATH, (err, token) => {
+      if (err) return getAccessToken(oAuth2Client, callback);
+      oAuth2Client.setCredentials(JSON.parse(token));
+      callback(oAuth2Client);
+    });
+  }
+
+  /**
+   * Get and store new token after prompting for user authorization, and then
+   * execute the given callback with the authorized OAuth2 client.
+   * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+   * @param {getEventsCallback} callback The callback for the authorized client.
+   */
+  function getAccessToken(oAuth2Client, callback) {
+    const authUrl = oAuth2Client.generateAuthUrl({
+      access_type: "offline",
+      scope: SCOPES,
+    });
+    console.log("Authorize this app by visiting this url:", authUrl);
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    rl.question("Enter the code from that page here: ", (code) => {
+      rl.close();
+      oAuth2Client.getToken(code, (err, token) => {
+        if (err) return console.error("Error retrieving access token", err);
+        oAuth2Client.setCredentials(token);
+        // Store the token to disk for later program executions
+        fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+          if (err) return console.error(err);
+          console.log("Token stored to", TOKEN_PATH);
+        });
+        callback(oAuth2Client);
+      });
+    });
+  }
+
+  /**
+   * Lists the next 10 events on the user's primary calendar.
+   * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+   */
+  function listEvents(auth) {
+    const calendar = google.calendar({ version: "v3", auth });
+
+    var event = {
+      summary: "Test Api",
+      location: "Bari",
+      description: "Siamo troppo forti",
+      start: {
+        dateTime: "2021-05-18T09:00:00-07:00",
+        timeZone: "America/Los_Angeles",
+      },
+      end: {
+        dateTime: "2021-05-18T17:00:00-07:00",
+        timeZone: "America/Los_Angeles",
+      },
+      recurrence: ["RRULE:FREQ=DAILY;COUNT=2"],
+      attendees: [{ email: "nunzio.gianfelice@apuliasoft.com" }],
+      reminders: {
+        useDefault: false,
+        overrides: [
+          { method: "email", minutes: 24 * 60 },
+          { method: "popup", minutes: 10 },
+        ],
+      },
+    };
+
+    calendar.events.insert(
+      {
+        calendarId: "166es9g3ipji45cuk4fobo8msc@group.calendar.google.com",
+        resource: event,
+      },
+      function (err, event) {
+        if (err) {
+          console.log(
+            "There was an error contacting the Calendar service: " + err
+          );
+          return;
+        }
+        console.log("Event created: %s", event.htmlLink);
+      }
+    );
+  }
+}
+
 (async () => {
   // Start the app
 
@@ -292,3 +407,8 @@ function capitalize(name) {
 
   console.log("⚡️ Bolt app is running!");
 })();
+
+module.exports = {
+  SCOPES,
+  listEvents,
+};
